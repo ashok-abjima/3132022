576f9153f2fed8f76d06e52239c5cd913b15f83e
Ashok Bajiya <88234185+ashok-abjima@users.noreply.github.com>
2022-03-31 23:07:15 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+   abstract class Bird
+    {
+        public void Fly()
+
+        {
+        Console.WriteLine("Fly in sky");
+
+        }
+    public abstract void Colour();
+    public abstract void Eat();
+
+    }
+class Parrot :Bird
+
+{
+    public override void Colour()
+    {
+        Console.WriteLine("Green");
+    }
+
+    public override void Eat()
+    {
+        Console.WriteLine("Chilly");
+        Fgh();
+    }
+    public void Fgh()
+    {
+        Console.WriteLine("Hey");
+    }
+    static void Main (string[]args)
+    {
+        Bird obj = new Parrot();
+        obj.Colour();
+        obj.Eat();
+        obj.Fly();
+
+        Console.Read();
+
+    }
+}
